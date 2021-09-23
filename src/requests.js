20e6c29110f50989647536be7826eb6494b9da80
Sharooque7 <sharooquethoufiq@gmail.com>
2021-09-23 19:36:27 +0530

    All Files

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "327fdd8a478ccc22f6103cc4386280d3";
+const API_KEY = process.env.REACT_APP_BASE_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
